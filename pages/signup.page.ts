413d3cd91dd90746140162fdf00178a5ea5d499b
feandev <jarmakowicz@gmail.com>
2024-02-23 15:22:02 +0100

    added sign up method

--- pages/signup.page.ts
+++ pages/signup.page.ts
@@ -7,4 +7,11 @@ export class SignUp {
   signUpUsername = this.page.getByLabel("Username:");
   signUpPassword = this.page.getByLabel("Password:");
   singUpButton = this.page.getByRole("button", { name: "Sign up" });
+
+  async singUpAction(userName: string, userPassword: string) {
+    await this.signUpLink.click();
+    await this.signUpUsername.fill(userName);
+    await this.signUpPassword.fill(userPassword);
+    await this.singUpButton.click();
+  }
 }
--- tests/01-signup.spec.ts
+++ tests/01-signup.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from "@playwright/test";
 import { SignUp } from "../pages/signup.page";
-import { userData } from "../test-data/user.data";
+import { loginData } from "../test-data/login.data";
 
 test("successful sign up", async ({ page }) => {
   await page.goto("/");
 
-  const userName = userData.userName;
-  const userPassword = userData.userPassword;
+  const userName = loginData.userName;
+  const userPassword = loginData.userPassword;
   const signUp = new SignUp(page);
 
-  await signUp.signUpLink.click();
-  await signUp.signUpUsername.fill(userName);
-  await signUp.signUpPassword.fill(userPassword);
-  await signUp.singUpButton.click();
+  await signUp.singUpAction(userName, userPassword);
+
 });
