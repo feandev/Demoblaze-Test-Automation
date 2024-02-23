c74ff1bacf3ccacab9fcba4e2c153a13a19cde28
feandev <jarmakowicz@gmail.com>
2024-02-23 15:24:27 +0100

    renamed user data file to login data

--- test-data/login.data.ts
+++ test-data/login.data.ts
@@ -1,4 +1,4 @@
-export const userData = {
+export const loginData = {
   userName: "John Playwright",
   userPassword: "playitright",
 };
--- tests/02-login.spec.ts
+++ tests/02-login.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
-import { userData } from "../test-data/user.data";
+import { loginData } from "../test-data/login.data";
 import { LoginPage } from "../pages/login.page";
 
 test("successful login", async ({ page }) => {
   await page.goto("/");
 
-  const userName = userData.userName;
-  const userPassword = userData.userPassword;
+  const userName = loginData.userName;
+  const userPassword = loginData.userPassword;
   const expectedWelcomeMsg = `Welcome ${userName}`;
   const loginPage = new LoginPage(page);
 
